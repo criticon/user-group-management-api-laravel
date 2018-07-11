b9eb97e42ed60659e868aafb7905cb9c521e9aca
Evgeniy Yaroshenko <criticon@yandex.ru>
2018-07-11 13:37:19 +0300

    User entity was modified.

---
+++ database/migrations/2018_07_11_090204_add_missing_fields_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMissingFieldsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name', 100)->after('id');
+            $table->string('last_name', 100)->after('first_name');
+            $table->enum('state', ['active', 'non active'])->after('last_name');
+            $table->dropColumn('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['first_name', 'last_name', 'state']);
+            $table->string('name', 255)->after('id');
+        });
+    }
+}
